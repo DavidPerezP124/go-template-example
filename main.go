5478cf3e20fd3686b0d90b3bcb2e1bc2056a89dd
David Perez <32963483+DavidPerezP124@users.noreply.github.com>
2023-02-01 02:42:43 +0000

    renames and documentation

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 )
 
 var (
-	port   = "8080"
+	port   = "8080" // The default PORT to serve this
 	albums = []album{
 		{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99, Poster: "https://m.media-amazon.com/images/I/71aSnR4uTNL._AC_SX385_.jpg", Sample: "https://storage.highresaudio.com/library/audio/c_190000/197842/format110516.mp3"},
 		{ID: "2", Title: "Briefcase Full Of Blues", Artist: "Blues Brothers", Price: 17.99, Poster: "https://ia801806.us.archive.org/18/items/lp_briefcase-full-of-blues_the-blues-brothers/lp_briefcase-full-of-blues_the-blues-brothers_itemimage.png", Sample: "https://ia801806.us.archive.org/18/items/lp_briefcase-full-of-blues_the-blues-brothers/disc1/02.03.%20Soul%20Man_sample.mp3"},
-		{ID: "3", Title: "Cowboy Bebop OST", Artist: "Various", Price: 39.99, Poster: "https://ia902305.us.archive.org/35/items/Cowboy-Bebop-OST-Collection/810VrvZrQsL._SL1442_.jpg?cnt=0", Sample: "https://ia802305.us.archive.org/35/items/Cowboy-Bebop-OST-Collection/Cowboy%20Bebop%20OST%20Collection/Cowboy%20Bebop%20CD-BOX%20Original%20Sound%20Track%20Limited%20Edition/Disc%201/02.%20Seatbelts%20-%20Tank%21%20%28TV%20Edit%29.mp3"},
+		{ID: "3", Title: "Cowboy Bebop OST", Artist: "Various", Price: 39.99, Poster: "https://ia902305.us.archive.org/35/items/Cowboy-Bebop-OST-Collection/810VrvZrQsL._SL1442_.jpg?cnt=0", Sample: "https://ia802305.us.archive.org/35/items/Cowboy-Bebop-OST-Collection/Cowboy%20Bebop%20OST%20Collection/Cowboy%20Bebop%20CD-BOX%20Original%20Sound%20Track%20Limited%20Edition/Disc%201/02.%20Seatbelts%20-%20Tank%21%20%28TV%20Edit%29.mp3"}, // The initial albums, can be expaneded using post methods.
 	}
 )
 
@@ -19,7 +19,7 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
-	router.GET("/", homeHandler)
+	router.GET("/", getHome)
 
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
@@ -41,13 +41,14 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-// albums slice to seed record album d
-
+// Adds a new album struct marshalled from the POST body.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-func homeHandler(c *gin.Context) {
+// The handler for the root path.
+func getHome(c *gin.Context) {
+	// Loads the index template and adds the initial information.
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"title":    "The Vinyl Co.",
 		"Subtitle": "Enjoy the best curated collection of Vinyl albums. The Vinyl Co. now offers a wide array of Vinyl albums at the reach of your fingertips, come and enjoy some classics with us.",
@@ -56,8 +57,8 @@ func homeHandler(c *gin.Context) {
 	})
 }
 
-// / Handles POST requests
-// / Bind the content body to a JSON Marsheller for
+// Handles POST requests
+// Bind the content body to a JSON marshalled from the album struct.
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
